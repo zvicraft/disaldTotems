34b88150ac0bfd8d3a6f3d77d3084a47aae8349f
zvicraft <zvicraft@gmail.com>
2024-07-16 18:34:38 +0300

    Initial commit - convert to kotlin

---
+++ src/main/kotlin/com/zvicraft/disaldtotems/DisaldTotems.kt
@@ -0,0 +1,13 @@
+package com.zvicraft.disaldtotems
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class DisaldTotems : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
